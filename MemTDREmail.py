31d8686e1c426085199494c4b648aa012841d89a
Chase Moxley <chasemoxley@outlook.com>
2019-05-29 15:28:26 -0500

    Added logging, Also doesn't send email if there wasn't activity the day before

--- MemTDREmail.py
+++ MemTDREmail.py
@@ -6,35 +6,55 @@ import io
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import smtplib
+import logging
 warnings.filterwarnings('ignore', 'Unverified HTTPS request')
+url = "https://protect.cylance.com/Reports/ThreatDataReportV1/memoryprotection/"
+token = "Token"
+fullurl = (url + token)
+path = 'Filepath'
+logfile = 'FilePath'
 nv = arrow.now()
 date = nv.shift(days=-1).format('M/D/YYYY')
 date2 = nv.shift(days=-1).format('YYYYMD')
-url = "https://protect.cylance.com/Reports/ThreatDataReportV1/memoryprotection/"
-token = "YourToken"
-fullurl = (url + token)
-path = 'File_Path'
-
 def email_send(email_data):
-    from_addr = "From email"
-    to_addr = "To email address"
-    to_list = [List of addresses to send email to ]
+    from_addr = "EmailFrom"
+    to_addr = "EmailTo"
+    to_list = ["To_List"]
     msg = MIMEMultipart()
     msg['From'] = from_addr
     msg['To'] = to_addr
     msg['Subject'] = "Cylance Exploit Attempts for %s" %(date)
     part2 = MIMEText(email_data, 'html')
     msg.attach(part2)
-    server = smtplib.SMTP("SmtpRelayAaddress", 25)
+    server = smtplib.SMTP("smtpRelay", 25)
     server.sendmail(from_addr,to_list,msg.as_string())
     server.quit()
 
-urlData = requests.get(fullurl).content
-rawData = pd.read_csv(io.StringIO(urlData.decode('utf-8')))
-df2 = pd.DataFrame(rawData)
-df3 = df2.drop(["Serial Number",], axis = 1)
-test3 = (df3[df3['ADDED'].str.contains(date)])
-output = (test3[["Device Name","ADDED",'PROCESS NAME','ACTION','TYPE','USER NAME']])
-output.to_csv(path+date2+"mem.csv", index = False)
-email_data = output.to_html(index = False)
-email_send(email_data)
+if __name__ == '__main__':
+
+    logging.basicConfig(filename=logfile, level=logging.INFO, format='%(asctime)s %(levelname)-8s %(message)s')
+    logging.info('Requesting MEM TDR')
+    urlData = requests.get(fullurl).content
+    rawData = pd.read_csv(io.StringIO(urlData.decode('utf-8')))
+    logging.info('Creating dataframe')
+    df2 = pd.DataFrame(rawData)
+    logging.info('Dropping Serial Column')
+    df3 = df2.drop(["Serial Number",], axis = 1)
+    logging.info('Filtering Data by date')
+    test3 = (df3[df3['ADDED'].str.contains(date)])
+    logging.info('Selecting Column Headers')
+    output = (test3[["Device Name","ADDED",'PROCESS NAME','ACTION','TYPE','USER NAME']])
+    print(output)
+
+    if output.empty:
+        logging.info('No Memory Exploit for %s' % (date))
+
+    else:
+        logging.info('Creating CSV')
+        output.to_csv(path + date2 + "mem.csv", index=False)
+        logging.info('CSV Created')
+        logging.info('Converting Data to HTML')
+        email_data = output.to_html(index = False)
+        logging.info('Preparing Email')
+        email_send(email_data)
+        logging.info('Email Sent')
